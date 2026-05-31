b73c644f6b5a79d0db6ee4540c4dbeee448440fc
Dan Marvin M, Isles <zdanmarvin@gmail.com>
2026-05-31 12:25:33 +0000

    head

--- backend/src/main/java/com/anipals/backend/social/entity/Friendship.java
+++ backend/src/main/java/com/anipals/backend/social/entity/Friendship.java
@@ -1,6 +1,17 @@
 package com.anipals.backend.social.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
